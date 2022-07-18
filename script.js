0a9b016ac1ebf42bd9e932f8a4e2b40e9e109832
Abhilipsa Sahoo <abhilipsasahoo03@gmail.com>
2022-07-18 18:04:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const WORDS=wordList.toString().split(',');
 const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
-var guessedWords = [];
+let guessedWords = [];
 let nextLetter = 0;
 let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
 console.log(rightGuessString);
